39daa3491210a76178692bd9c0b420dd8afb24a1
egamovhamid <egamovhamid@gmail.com>
2023-09-01 21:10:16 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fitted Box',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Fitted Box'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _typedText = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (text) {
+                setState(() {
+                  if (text.isEmpty) {
+                    _typedText = 'Hello World!';
+                  } else {
+                    _typedText = text;
+                  }
+                });
+              },
+            ),
+            const SizedBox(height: 100),
+            FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Text(
+                _typedText,
+                style: const TextStyle(fontSize: 100),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
